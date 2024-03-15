9213184153957804174dedeb3d8fa5800e30dfd8
yusuke-kawara <happywanwan2007@gmail.com>
2024-03-15 20:22:00 +0900

    検索機能完了
    連絡事項とそれを投稿したユーザーの検索ができるように設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,8 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
+    @q = Post.ransack(params[:q])
+    @posts = @q.result(distinct: true).includes(:user).order(created_at: :desc).page(params[:page])
   end
 
   # GET /posts/1 or /posts/1.json
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  # 検索可能な属性を指定
+  def self.ransackable_attributes(auth_object = nil)
+    %w[title content]
+  end
+
+  # 検索可能な関連を指定
+  def self.ransackable_associations(auth_object = nil)
+    %w[user]
+  end
 end
