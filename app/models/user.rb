a48f380ff03177951472411e38d6f0af6af65969
yusuke-kawara <happywanwan2007@gmail.com>
2024-03-16 08:32:15 +0900

    検索機能完了②
    管理者ユーザー、一般ユーザー一人づつ作成してある状態

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[name]
+  end
 end
