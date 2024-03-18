1453d837760bb18d4b1c041dc0a41d5aa7b8ef6c
yusuke-kawara <happywanwan2007@gmail.com>
2024-03-18 15:44:12 +0900

    i18n日本語化の漏れの修正、投稿のタイトル、本文のバリデーション設定
    validates :title, presence: true, length: { maximum: 50 }
    validates :content, presence: true, length: { maximum: 2000 }

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-  validates :title, presence: true
-  validates :content, presence: true
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :content, presence: true, length: { maximum: 2000 }
   belongs_to :user
 
   # 検索可能な属性を指定
