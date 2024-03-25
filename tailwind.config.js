3d3a0d2be26cd93dd9071b49ac077371e59a5470
yusuke-kawara <happywanwan2007@gmail.com>
2024-03-25 09:18:14 +0900

    クレデンシャルファイル関係のエラー解決中
    本番環境と開発環境のクレデンシャルファイルが分かれてしまっているためひとつにしたい

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/views/**/*.{html,html.erb}", // Rails views ディレクトリ内のファイル
-    "./app/javascript/**/*.js", // JavaScriptファイルのパス
-    // 必要に応じて他のディレクトリやファイルタイプを追加
+    "./app/views/**/*.{html,erb,html.erb}",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    // 他にもスキャンしたいファイルやディレクトリがあれば追加
   ],
   theme: {
     extend: {},
